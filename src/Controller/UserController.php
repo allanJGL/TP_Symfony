e13d316f71e1c990ba08cfaeb5815c97cd3c537a
e-guyot <guyot.estellee@gmail.com>
2020-02-03 15:37:56 +0100

    add delete user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -118,4 +118,25 @@ class UserController extends AbstractController
             'users' => $users,
         ));
     }
+
+    /**
+     * Delete a userity.
+     *
+     * @Route("/delete/user/{id}", name="deleteUser")
+     *
+     */
+    public function delete(Request $request, $id)
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'User tried to access a page without having ROLE_ADMIN');
+        if (!$id) {
+            throw $this->createNotFoundException('No user found');
+        }
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->find(User::class, $id);
+        $em->remove($user);
+        $em->flush();
+        $this->addFlash('success', $user->getFirstName() . ' was deleted.');
+
+        return $this->redirectToRoute('home');
+    }
 }
